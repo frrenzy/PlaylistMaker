f69038fcdb88232e91200c73266e08df65ca3ba0
Ivan Sizov <vsizov63@gmail.com>
2026-02-08 20:04:54 +0300

    task #2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +23,42 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton = findViewById<ImageView>(R.id.settings_back_button)
         backButton.setOnClickListener { finish() }
+
+        val shareButton = findViewById<LinearLayout>(R.id.settings_share)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.share_message)
+            )
+
+            startActivity(
+                Intent.createChooser(
+                    shareIntent,
+                    getString(R.string.share_title)
+                )
+            )
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.settings_support)
+        supportButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_message))
+
+            startActivity(shareIntent)
+        }
+
+        val agreementButton = findViewById<LinearLayout>(R.id.settings_agreement)
+        agreementButton.setOnClickListener {
+            Uri.parse(getString(R.string.agreement_link)).let { webpage ->
+                val viewIntent = Intent(Intent.ACTION_VIEW, webpage)
+
+                startActivity(viewIntent)
+            }
+        }
     }
 }
